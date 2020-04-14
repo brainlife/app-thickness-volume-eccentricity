f419fb76cf6c0dd7841b1d732b3f699aebabfdbd
Brad Caron <bacaron@iu.edu>
2020-04-13 22:58:36 -0400

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -10,5 +10,5 @@ do
 	mris_convert $freesurfer/surf/${HEMI}.pial ./${HEMI}.pial.surf.gii
 	mris_convert -c $freesurfer/surf/${HEMI}.volume ./${HEMI}.pial.surf.gii ./${HEMI}.volume.surf.gii
 	mris_convert -c $freesurfer/surf/${HEMI}.thickness ./${HEMI}.pial.surf.gii ./${HEMI}.thickness.surf.gii
-	mris_convert -c $prf/${HEMI}.benson14_eccen ./${HEMI}.pial.surf.gii ./${HEMI}.benson14_eccen.surf.gii
+	mris_convert -c $prf/${HEMI}.eccentricity ./${HEMI}.pial.surf.gii ./${HEMI}.benson14_eccen.surf.gii
 done
