694240e4a906efc8eaf384970bb6943d5aafd320
Bradley Caron <bacaron@h1.carbonate.uits.iu.edu>
2020-04-21 18:24:02 -0400

    reason: fix

--- build.m
+++ build.m
@@ -1,4 +1,5 @@
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/spm12'))
+gifti();
 mcc -m -R -nodisplay -a /N/u/brlife/git/spm12/@gifti -d compiled eccentricityVolumeThickness
 exit
