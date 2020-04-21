66e92c91dd96cf7161449c60967499e4428f3154
Soichi Hayashi <hayashis@iu.edu>
2020-04-21 23:28:15 +0000

    added compile path for @gitfi/private

--- build.m
+++ build.m
@@ -1,5 +1,4 @@
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/spm12'))
-gifti('test.gii');
-mcc -m -R -nodisplay -a /N/u/brlife/git/spm12/@gifti -d compiled eccentricityVolumeThickness
+mcc -m -R -nodisplay -I /N/u/brlife/git/spm12/@gifti/private -d compiled eccentricityVolumeThickness
 exit
