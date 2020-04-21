7b893876d1889645623664d2729d7d968aa9c669
Bradley Caron <bacaron@h1.carbonate.uits.iu.edu>
2020-04-21 18:18:39 -0400

    REason: fix

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@ mkdir compiled
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/spm12'))
-mcc -m -R -nodisplay -d compiled eccentricityVolumeThickness
+mcc -m -R -nodisplay -a /N/u/brlife/git/spm12/@gifti -d compiled eccentricityVolumeThickness
 exit
 END
 matlab -nodisplay -nosplash -r build
